80eb5c294174497a64d20d1e3df3d79c63e409f7
Matt Budde <matt@designbymatt.org>
2018-02-14 14:06:59 +0000

    Code formatting

--- main.js
+++ main.js
@@ -1,51 +1,59 @@
-const {app, Menu, BrowserWindow, ipcMain, Tray, nativeImage, systemPreferences} = require('electron');
-const path = require('path');
-const assetsDir = path.join(__dirname, 'assets');
-const {appUpdater} = require('./assets/js/autoupdater');
-const isDev = require('electron-is-dev');
-const powerSaveBlocker = require('electron').powerSaveBlocker;
+const {
+  app,
+  Menu,
+  BrowserWindow,
+  ipcMain,
+  Tray,
+  nativeImage,
+  systemPreferences
+} = require("electron");
+const path = require("path");
+const assetsDir = path.join(__dirname, "assets");
+const { appUpdater } = require("./assets/js/autoupdater");
+const isDev = require("electron-is-dev");
+const powerSaveBlocker = require("electron").powerSaveBlocker;
 
-const openAboutWindow = require('about-window').default;
+const openAboutWindow = require("about-window").default;
 
-const darkIcon = path.join(__dirname,'assets/img/icon-dark.png')
-const lightIcon = path.join(__dirname,'assets/img/icon.png')
+const darkIcon = path.join(__dirname, "assets/img/icon-dark.png");
+const lightIcon = path.join(__dirname, "assets/img/icon.png");
 
 let tray = undefined;
 let window = undefined;
 
 // This method is called once Electron is ready to run our code
 // It is effectively the main method of our Electron app
-app.on('ready', () => {
+app.on("ready", () => {
   //Check for updates every 15 Minutes
   setInterval(function() {
     appUpdater();
-  }, 900000)
+  }, 900000);
   // Prevent App being put to sleep
-  powerSaveBlocker.start('prevent-app-suspension');
+  powerSaveBlocker.start("prevent-app-suspension");
   // Setup the menubar with an icon
-  if(systemPreferences.isDarkMode(true)) {
+  if (systemPreferences.isDarkMode(true)) {
     var icon = darkIcon;
-    } else {
-        var icon = lightIcon;
-    };
-  tray = new Tray(icon)
+  } else {
+    var icon = lightIcon;
+  }
+  tray = new Tray(icon);
   //Set Menu of App
   Menu.setApplicationMenu(menu);
   // Add a click handler so that when the user clicks on the menubar icon, it shows
   // our popup window
-  tray.on('click', function(event) {
+  tray.on("click", function(event) {
     toggleWindow();
     //dev tools
     //window.webContents.openDevTools({mode: 'undocked'});
-  })
+  });
 
-  ipcMain.on('open-window', () => {
+  ipcMain.on("open-window", () => {
     showWindow();
   });
 
-  ipcMain.on('close-window', () => {
+  ipcMain.on("close-window", () => {
     window.hide();
-  })
+  });
 
   // Make the popup window for the menubar
   window = new BrowserWindow({
@@ -53,70 +61,71 @@ app.on('ready', () => {
     height: 200,
     show: false,
     frame: false,
-    resizable: false,
-  })
+    resizable: false
+  });
 
   // Tell the popup window to load our index.html file
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+  window.loadURL(`file://${path.join(__dirname, "index.html")}`);
 
   // Only close the window on blur if dev tools isn't opened
-  window.on('blur', () => {
-    if(!window.webContents.isDevToolsOpened()) {
-      window.hide()
+  window.on("blur", () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide();
     }
-  })
-})
+  });
+});
 
 const toggleWindow = () => {
   if (window.isVisible()) {
-    window.hide()
+    window.hide();
   } else {
-    showWindow()
+    showWindow();
   }
-}
+};
 
 const showWindow = () => {
-  const trayPos = tray.getBounds()
-  const windowPos = window.getBounds()
-  let x, y = 0
-  if (process.platform == 'darwin') {
-    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
-    y = Math.round(trayPos.y + trayPos.height)
+  const trayPos = tray.getBounds();
+  const windowPos = window.getBounds();
+  let x,
+    y = 0;
+  if (process.platform == "darwin") {
+    x = Math.round(trayPos.x + trayPos.width / 2 - windowPos.width / 2);
+    y = Math.round(trayPos.y + trayPos.height);
   } else {
-    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
-    y = Math.round(trayPos.y + trayPos.height * 10)
+    x = Math.round(trayPos.x + trayPos.width / 2 - windowPos.width / 2);
+    y = Math.round(trayPos.y + trayPos.height * 10);
   }
 
-  window.setPosition(x, y, false)
-  window.show()
-  window.focus()
-}
+  window.setPosition(x, y, false);
+  window.show();
+  window.focus();
+};
 
-ipcMain.on('show-window', () => {
-  showWindow()
-})
+ipcMain.on("show-window", () => {
+  showWindow();
+});
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 //About this app menu build
 const menu = Menu.buildFromTemplate([
   {
-    label: 'Example',
+    label: "Example",
     submenu: [
-          {
-            label: 'About This App',
-              click: () =>
-                  openAboutWindow({
-                    icon_path: path.join(__dirname, '/assets/img/logo.png'),
-                    copyright: 'Copyright (c) 2018 Matt Budde',
-                    package_json_dir: __dirname,
-                }),
-            },
-        ],
-    },
+      {
+        label: "About This App",
+        click: () =>
+          openAboutWindow({
+            icon_path: path.join(__dirname, "/assets/img/logo.png"),
+            copyright: "Copyright (c) 2018 Matt Budde",
+            package_json_dir: __dirname
+          })
+      }
+    ]
+  }
 ]);
--- renderer.js
+++ renderer.js
@@ -1,12 +1,12 @@
-const {ipcRenderer} = require('electron');
-const notifier = require('node-notifier');
+const { ipcRenderer } = require("electron");
+const notifier = require("node-notifier");
 var nc = new notifier.NotificationCenter();
-const path = require('path');
+const path = require("path");
 
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelectorAll('[data-time]');
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
   // clear any existing timers
@@ -16,47 +16,46 @@ function timer(seconds) {
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
 
-  if(seconds >= 1500) {
+  if (seconds >= 1500) {
     endTime.textContent = `Get to work!`;
     timerNotification(seconds);
-  } else{
+  } else {
     displayEndTime(then);
   }
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if we should stop it!
-    if(secondsLeft == 0) {
-      let trueAnswer = 'Keep Going';
-      let label = 'Break';
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      let trueAnswer = "Keep Going";
+      let label = "Break";
 
       nc.notify(
         {
-          title: 'Ding!',
-          message: 'Keep going or take a break',
-          sound: 'Funk',
-          icon: path.join(__dirname, 'assets/img/logo.png'),
+          title: "Ding!",
+          message: "Keep going or take a break",
+          sound: "Funk",
+          icon: path.join(__dirname, "assets/img/logo.png"),
           // case sensitive
-          closeLabel: label,
-          actions: trueAnswer
+          actions: trueAnswer,
+          closeLabel: label
         },
         function(err, response, metadata) {
           if (err) throw err;
           console.log(metadata);
 
           if (metadata.activationValue == trueAnswer) {
-            var pomo = document.querySelector('#pomo');
+            const pomo = document.querySelector("#pomo");
             seconds = parseInt(pomo.dataset.time);
             timer(seconds);
-            
-          } else if(metadata.activationValue == label) {
-            const shortBreak = document.querySelector('#short__break');
+          } else if (metadata.activationValue == label) {
+            const shortBreak = document.querySelector("#short__break");
             seconds = parseInt(shortBreak.dataset.time);
             timer(seconds);
-          } 
+          }
         }
       );
-      clearInterval(countdown)
     }
     displayTimeLeft(secondsLeft);
   }, 1000);
@@ -65,7 +64,9 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
@@ -74,7 +75,9 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = `Be Back At ${hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
 }
 
 function startTimer() {
@@ -83,15 +86,15 @@ function startTimer() {
 }
 
 function timerNotification(seconds) {
-    let n = new Notification('Timer started!',{
-        body: 'Gotta go fast',
-    });
+  let n = new Notification("Timer started!", {
+    body: "Gotta go fast"
+  });
 }
 
-buttons.forEach(button => button.addEventListener('click', startTimer));
+buttons.forEach(button => button.addEventListener("click", startTimer));
 /*document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const mins = this.minutes.value;
   timer(mins * 60);
   this.reset();
-});*/ 
+});*/
